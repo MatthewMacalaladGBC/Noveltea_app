e4575808ef31ed9ee5dc8dfae6fff9d8faa7f202
Sara <sara.yohannes94@gmail.com>
2026-02-18 20:37:55 -0500

    Rename UsersController to UserController

--- backend/src/main/java/com/noveltea/backend/controller/UserController.java
+++ backend/src/main/java/com/noveltea/backend/controller/UserController.java
@@ -9,11 +9,11 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
-public class UsersController {
+public class UserController {
 
     private final UserService userService;
 
-    public UsersController(UserService userService) {
+    public UserController(UserService userService) {
         this.userService = userService;
     }
 
